836af82e88909fe0582a6deaa9071686de1de26e
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-19 12:26:29 +0200

    ClaseContrato

--- contractTest.js
+++ contractTest.js
@@ -1,6 +1,6 @@
-let myLib = require("./contract")
+let myLib3 = require("./contract")
 
-let pepe = new myLib.Person("Pepe", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
-let array = new myLib.Contacts()
+let pepe = new myLib3.myLib2.Person("Pepe", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
+let array = new myLib3.Contacts()
 array.persona.push(pepe)
 array.printPersons();
