37c3ace1db834f8316280ccfc7257b339451d2ba
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-19 12:56:40 +0200

    ClasePersona

--- person.ts
+++ person.ts
@@ -1,1 +1,18 @@
-class Person{}
+class Person{
+    public name: string;
+    public age: number;
+    private adress: string;
+    constructor(name:string, age:number, adress:string){
+        this.name = name;
+        this.age = age
+        this.adress = adress
+    }
+/////metodos
+public printName(){
+    return this.name
+}
+
+    }
+
+let pepe = new Person('Juan', 15, 'Primo de Rivera')
+console.log(pepe.printName());
