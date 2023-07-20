b19d0f14610c79e0709b1256cc0a99041aa5d380
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-20 15:48:55 +0300

    Reto

--- personTest.js
+++ personTest.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var person_1 = require("./person");
+var person_1 = require("./poo/person");
 var pepe = new person_1.Person('Juan', 20, 'Gran Vía');
 console.log(pepe.printName());
 console.log(pepe.yearOfBirth(2023));
