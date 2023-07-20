f43bbaa5de2f77b65b9efce8012efcdc198718eb
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-20 15:28:05 +0300

    Reto

--- contactsTest.ts
+++
@@ -1,8 +0,0 @@
-import { Person } from "./person";
-import { Contacts } from "./contacts";
-
-let pepe = new Person("Juan", 20, "Gran Via");
-let array = new Contacts();
-array.people.push(pepe)
-array.printCalendar()
-
