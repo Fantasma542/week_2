0072084a1fb7ef47085b4b75967909367465e363
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-20 15:01:48 +0300

    ClaseContact

--- poo/contactTest.js
+++ poo/contactTest.js
@@ -1,4 +1,4 @@
-let myLib3 = require("./poo/contact")
+let myLib3 = require("./contact")
 
 let pepe = new myLib3.myLib2.Person("Pepe", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
 let array = new myLib3.Contacts()
