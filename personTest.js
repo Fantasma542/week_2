12cc4b9a49ec4baf720bd89bdd175c2c29e2899e
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-20 15:57:23 +0300

    Hola

--- personTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var person_1 = require("./poo/person");
-var pepe = new person_1.Person('Juan', 20, 'Gran Vía');
-console.log(pepe.printName());
-console.log(pepe.yearOfBirth(2023));
-pepe.setAddress('Puerta del Sol');
-console.log(pepe);
-console.log(pepe.getAddress());
