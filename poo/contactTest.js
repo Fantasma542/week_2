0d49c03b5a92a8cc83e646bab40706bf650772c4
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-21 15:15:55 +0300

    retoGit

--- poo/contactTest.js
+++
@@ -1,6 +0,0 @@
-let myLib3 = require("./contact")
-
-let pepe = new myLib3.myLib2.Person("Pepe", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
-let array = new myLib3.Contacts()
-array.persona.push(pepe)
-array.printPersons();
