65c5cecbd6f42834a372ea72b1eaecd812ebe170
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-24 15:01:58 +0300

    ContactTest

--- poo/contactTest.js
+++ poo/contactTest.js
@@ -1,6 +1,6 @@
 let myLib3 = require("./contact")
 
-let pepe = new myLib3.myLib2.Person("Pepe", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
+let pepe = new myLib3.myLib2.Person("Pepes", "Gonzalez", 1993, 65, 1.75, ['Correr', 'Dormir', 'Nadar'])
 let array = new myLib3.Contacts()
 array.persona.push(pepe)
 array.printPersons();
