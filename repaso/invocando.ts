bad9e5ca32abdf487b58b104a7061429a3e746c5
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-24 13:12:53 +0300

    Invocando

--- repaso/invocando.ts
+++ repaso/invocando.ts
@@ -6,10 +6,3 @@ let arrays = [
     ["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"],
     ["Venezuela", "Veneno", "Voltaje"]
   ];
-
-for (let i = 0; i < arrays.length; i++) {
-    let array = arrays[i];
-    let suma = add(array);
-    isEven(suma);
-  console.log(`Su suma es: ${suma}`);
-}
