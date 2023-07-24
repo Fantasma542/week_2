3985ad74eec45ae9f0579faebcb59ef467e5abfc
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-24 14:52:10 +0300

    RetosCodenotch

--- repaso/invocando.ts
+++ repaso/invocando.ts
@@ -6,8 +6,6 @@ let arrays = [
     ["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"],
     ["Venezuela", "Veneno", "Voltaje"]
   ];
-<<<<<<< HEAD
-=======
 
   let arr1 = add(arrays[0]);
   let arr2 = add(arrays[1]);
@@ -15,4 +13,3 @@ let arrays = [
   let parImp1 = isEven(arr1)
   let parImp2 = isEven(arr2)
   let parImp3 = isEven(arr3)
->>>>>>> repaso1
