02ed0b101a0ba6230817c8c997a22be9622a3809
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-24 14:33:28 +0300

    ClaseTriangle

--- algebraProject/triangleTest.js
+++ algebraProject/triangleTest.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var point_1 = require("./point");
 var triangle_1 = require("./triangle");
-// Creamos instancias de Point para representar los vértices del triángulo
 var vertice1 = new point_1.Point(1, 2);
 var vertice2 = new point_1.Point(4, 6);
 var vertice3 = new point_1.Point(-2, 7);
-// Creamos una instancia de Triangle utilizando los vértices creados anteriormente
 var myTriangle = new triangle_1.Triangle(vertice1, vertice2, vertice3);
 var longitudes = myTriangle.calculateLengthSides();
 console.log(longitudes);
--- algebraProject/triangleTest.ts
+++ algebraProject/triangleTest.ts
@@ -1,12 +1,10 @@
 import { Point } from "./point";
 import { Triangle } from "./triangle";
 
-// Creamos instancias de Point para representar los vértices del triángulo
 let vertice1 = new Point(1, 2);
 let vertice2 = new Point(4, 6);
 let vertice3 = new Point(-2, 7);
 
-// Creamos una instancia de Triangle utilizando los vértices creados anteriormente
 let myTriangle = new Triangle(vertice1, vertice2, vertice3);
 let longitudes = myTriangle.calculateLengthSides();
 console.log(longitudes);
