c658dc76b2dff95010e2604fec84547cf9ca3e45
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-07-24 14:10:45 +0300

    ClaseMobileCollection

--- mobileProyect/mobileCollectionTest.ts
+++ mobileProyect/mobileCollectionTest.ts
@@ -1,17 +1,17 @@
 import { Mobile } from "./mobile";
 import { MobileCollection } from "./mobileCollection";
 
-let mobile1 = new Mobile("SA", "SS", "Soy", "Rojo", 5)
-let mobile2 = new Mobile("Desfe", "No", "Si", "Azul", 50)
-let mobile3 = new Mobile("Donde", "El", "Po", "Marro", 30)
-let mobile4 = new Mobile("Pues", "Al", "Nre", "Negro", 20)
+let mobile1 = new Mobile("A12", "Galaxy", "Samsung", "Azul", 117)
+let mobile2 = new Mobile("S20", "Galaxy", "Samsung", "Amarillo", 270)
+let mobile3 = new Mobile ("S21", "Galaxy", "Samsung", "Rojo", 384)
+let mobile4 = new Mobile ("A54", "Galaxy", "Samsung", "Negro", 415)
 
 let mobiles = [mobile1, mobile2, mobile3, mobile4]
 
 let mobilesCollection = new MobileCollection(mobiles)
-// console.log(mobilesCollection);
+console.log(mobilesCollection);
 
-// console.log(mobilesCollection.getMobile());
+console.log(mobilesCollection.getMobile());
 
-// console.log(mobilesCollection.getTotalPrice());
+console.log(mobilesCollection.getTotalPrice());
 mobilesCollection.printCollection()
